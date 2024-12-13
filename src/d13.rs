d8dececc947d9f496b258459b2f67dfab43ab016
rjhallsted <rjhallsted@gmail.com>
2024-12-13 15:03:18 -0800

    Fix day 13 to just solve the system of equations

--- src/d13.rs
+++ src/d13.rs
@@ -5,16 +5,16 @@ use regex::Regex;
 
 #[derive(Eq, PartialEq, Hash, Debug, Clone)]
 struct Position {
-    x: usize,
-    y: usize,
+    x: i64,
+    y: i64,
 }
 impl Position {
-    fn new(x: usize, y: usize) -> Self {
+    fn new(x: i64, y: i64) -> Self {
         Position { x, y }
     }
 
     fn from_str(x: &str, y: &str) -> Self {
-        Self::new(x.parse::<usize>().unwrap(), y.parse::<usize>().unwrap())
+        Self::new(x.parse::<i64>().unwrap(), y.parse::<i64>().unwrap())
     }
 
     fn translate_by_pos(&mut self, pos: &Self) {
@@ -27,7 +27,7 @@ impl Position {
         self.y -= pos.y;
     }
 
-    fn maximum_interval(&self, int: &Self) -> usize {
+    fn maximum_interval(&self, int: &Self) -> i64 {
         (self.x / int.x).min(self.y / int.y)
     }
 
@@ -58,11 +58,11 @@ fn load_input(filename: &str) -> Vec<(Position, Position, Position)> {
     inputs
 }
 
-const PRIMES: [usize; 25] = [
+const PRIMES: [i64; 25] = [
     2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97,
 ];
 
-fn gcd(a: usize, b: usize) -> usize {
+fn gcd(a: i64, b: i64) -> i64 {
     if b == 0 {
         a
     } else {
@@ -70,7 +70,7 @@ fn gcd(a: usize, b: usize) -> usize {
     }
 }
 
-fn lcm(mut a: usize, mut b: usize) -> usize {
+fn lcm(mut a: i64, mut b: i64) -> i64 {
     //println!("lcm({a}, {b}) = ");
     for p in &PRIMES {
         if b < *p {
@@ -86,11 +86,60 @@ fn lcm(mut a: usize, mut b: usize) -> usize {
     a
 }
 
-fn valid(a_diff: &Position, b_diff: &Position, prize: &Position, a: usize, b: usize) -> bool {
+fn valid(a_diff: &Position, b_diff: &Position, prize: &Position, a: i64, b: i64) -> bool {
     (a * a_diff.x) + (b * b_diff.x) == prize.x && (a * a_diff.y) + (b * b_diff.y) == prize.y
 }
 
-fn cheapest_path_to_prize(a_diff: &Position, b_diff: &Position, prize: &Position) -> Option<usize> {
+struct LinearEquation {
+    a: i64,
+    b: i64,
+    c: i64,
+}
+impl LinearEquation {
+    fn new(a: i64, b: i64, c: i64) -> Self {
+        LinearEquation { a, b, c }
+    }
+
+    fn multiply_by(&self, x: i64) -> Self {
+        LinearEquation {
+            a: self.a * x,
+            b: self.b * x,
+            c: self.c * x,
+        }
+    }
+
+    fn subtract_equation(&self, other: &LinearEquation) -> Self {
+        LinearEquation {
+            a: self.a - other.a,
+            b: self.b - other.b,
+            c: self.c - other.c,
+        }
+    }
+}
+
+fn path_to_prize_fast_way(a_diff: &Position, b_diff: &Position, prize: &Position) -> Option<i64> {
+    let x_eq = LinearEquation::new(a_diff.x, b_diff.x, prize.x);
+    let y_eq = LinearEquation::new(a_diff.y, b_diff.y, prize.y);
+
+    let x_by_ay = x_eq.multiply_by(y_eq.a);
+    let y_by_ax = y_eq.multiply_by(x_eq.a);
+
+    let b_only_eq = x_by_ay.subtract_equation(&y_by_ax);
+    let b = b_only_eq.c / b_only_eq.b;
+
+    let a = (x_eq.c - (x_eq.b * b)) / x_eq.a;
+    if valid(a_diff, b_diff, prize, a, b) {
+        Some((a * 3) + b)
+    } else {
+        None
+    }
+}
+
+fn cheapest_path_to_prize_using_diophantine_equation_approach(
+    a_diff: &Position,
+    b_diff: &Position,
+    prize: &Position,
+) -> Option<i64> {
     println!("{a_diff:?}, {b_diff:?}, {prize:?}");
 
     let p = prize.x + prize.y;
@@ -115,7 +164,6 @@ fn cheapest_path_to_prize(a_diff: &Position, b_diff: &Position, prize: &Position
     if b * combined_b_diff > p {
         return None;
     }
-    let mut min: Option<usize> = None;
     let mut a = a_from_b(b);
     while a >= a_step {
         b = b_from_a(a);
@@ -132,18 +180,18 @@ fn cheapest_path_to_prize(a_diff: &Position, b_diff: &Position, prize: &Position
         return Some((3 * a) + b);
         //min = min.map(|m| m.min((3 * a) + b)).or(Some((3 * a) + b));
     }
-    min
+    None
 }
 
-fn sum_cheapest_paths(filename: &str) -> usize {
+fn sum_cheapest_paths(filename: &str) -> i64 {
     let input = load_input(filename);
     input
         .iter()
-        .filter_map(|(a, b, prize)| cheapest_path_to_prize(a, b, prize))
+        .filter_map(|(a, b, prize)| path_to_prize_fast_way(a, b, prize))
         .sum()
 }
 
-pub fn sum_cheapest_paths_part2(filename: &str) -> usize {
+pub fn sum_cheapest_paths_part2(filename: &str) -> i64 {
     let input = load_input(filename);
     println!("{} inputs", input.len());
     input
@@ -152,7 +200,7 @@ pub fn sum_cheapest_paths_part2(filename: &str) -> usize {
             prize.translate_by_pos(&Position::new(10000000000000, 10000000000000));
             (a, b, prize)
         })
-        .filter_map(|(a, b, prize)| cheapest_path_to_prize(&a, &b, &prize))
+        .filter_map(|(a, b, prize)| path_to_prize_fast_way(&a, &b, &prize))
         .sum()
 }
 
@@ -175,12 +223,11 @@ mod tests {
     #[test]
     fn part2_example() {
         let result = sum_cheapest_paths_part2("example.txt");
-        assert_eq!(result, 480);
+        assert_eq!(result, 875318608908);
     }
 
     #[test]
     fn part2_problem() {
-        panic!("Don't run in tests, takes way too long");
         let result = sum_cheapest_paths_part2("input.txt");
         assert_eq!(result, 82261957837868);
     }
